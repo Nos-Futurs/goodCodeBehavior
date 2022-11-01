fec8ef2cda6e2187b950a3b4e55f773242d7e2a9
PaulPEHARPRE <paul.peharpre@koji.fr>
2022-11-01 08:42:05 +0100

    first commit : set-Up

---
+++ webpack.config.js
@@ -0,0 +1,48 @@
+const prod = process.env.NODE_ENV === 'production';
+
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+const CopyPlugin = require("copy-webpack-plugin");
+
+module.exports = {
+  mode: prod ? 'production' : 'development',
+  entry: {
+    main: "./src/index.tsx",
+    content: './src/chrome/content.ts'
+  },
+  output: {
+    path: __dirname + '/build',
+    filename: 'static/js/[name].js'
+  },
+  module: {
+    rules: [
+      {
+        test: /\.(ts|tsx)$/,
+        exclude: /node_modules/,
+        resolve: {
+          extensions: ['.ts', '.tsx', '.js', '.json'],
+        },
+        use: 'ts-loader',
+      },
+      {
+        test: /\.css$/,
+        use: [MiniCssExtractPlugin.loader, 'css-loader'],
+      },
+    ]
+  },
+  devtool: prod ? undefined : 'source-map',
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: './src/index.html',
+    }),
+    new MiniCssExtractPlugin(),
+    new CopyPlugin({
+      patterns: [
+        { from: "public" },
+      ],
+    })
+  ],
+  optimization: {
+    runtimeChunk: false,
+  }
+};
