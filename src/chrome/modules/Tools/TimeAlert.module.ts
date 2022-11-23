c0aef0c04b2f83fc4b6de9da938f1072ef94c595
PaulPEHARPRE <paul.peharpre@koji.fr>
2022-11-23 15:08:26 +0100

    feat/alarm creation

--- src/chrome/modules/Tools/TimeAlert.module.ts
+++ src/chrome/modules/Tools/TimeAlert.module.ts
@@ -1,30 +1,33 @@
 const timeRules = "timeRulesObject";
+const lastActiveUrlDomain = "lastActivetab";
+const tabsTime = "tabsTimeObject";
 
 // Create new time rule
-export function createNewTimeRule(domain: string, time: number) {
-  createAlarm();
+export function createNewTimeRule(
+  domain: string,
+  time: number,
+  nextWarning: number = 0
+) {
   chrome.storage.local.get([timeRules], function (result) {
     const timeRulesJSON = result[timeRules];
-    console.log(timeRulesJSON);
     let timeRulesObject: any = {};
     if (timeRulesJSON !== undefined) {
       timeRulesObject = JSON.parse(timeRulesJSON);
     }
-    timeRulesObject[domain] = { domain, time };
+    timeRulesObject[domain] = { domain, time, nextWarning };
 
     const rulesTimeString = JSON.stringify(timeRulesObject);
     let newTabTimeObject: any = {};
     newTabTimeObject[timeRules] = rulesTimeString;
     chrome.storage.local.set(newTabTimeObject, function () {});
   });
+  createTimeAlarm();
 }
 
 // Delete time rule
 export function DeleteTimeRule(domain: string) {
-  createAlarm();
   chrome.storage.local.get([timeRules], function (result) {
     const timeRulesJSON = result[timeRules];
-    console.log(timeRulesJSON);
     let timeRulesObject: any = {};
     if (timeRulesJSON !== undefined) {
       timeRulesObject = JSON.parse(timeRulesJSON);
@@ -39,24 +42,76 @@ export function DeleteTimeRule(domain: string) {
   });
 }
 
-export const createAlarm = () => {
-  chrome.notifications.create(
-    "test",
-    {
-      type: "basic",
-      iconUrl: chrome.runtime.getURL("images/logo.png"),
-      title: "Time for cake!",
-      message: "Something something cake",
-    },
+export const notificationSending = () => {
+  chrome.storage.local.get(
+    [timeRules, lastActiveUrlDomain, tabsTime],
     function (result) {
-      console.log(result);
+      const timeRulesJSON = result[timeRules];
+      const lastActiveTabJSON = result[lastActiveUrlDomain];
+      const tabsTimeJSON = result[tabsTime];
+
+      if (
+        // If no time rules defined, then clear alarms
+        timeRulesJSON === undefined ||
+        JSON.stringify(timeRulesJSON) === "{}"
+      ) {
+        chrome.alarms.clearAll();
+      } else {
+        // If Time rules are defined
+        const timeRulesObject = JSON.parse(timeRulesJSON);
+
+        if (
+          // if time tracking is working
+          lastActiveTabJSON !== undefined &&
+          JSON.stringify(lastActiveTabJSON) !== "{}" &&
+          tabsTimeJSON !== undefined &&
+          JSON.stringify(tabsTimeJSON) !== "{}"
+        ) {
+          const tabTimeObject = JSON.parse(tabsTimeJSON);
+          const lastActiveTabObject = JSON.parse(lastActiveTabJSON);
+          for (let domain in timeRulesObject) {
+            if (lastActiveTabObject.url === domain) {
+              let timingRule =
+                timeRulesObject[domain].nextWarning &&
+                timeRulesObject[domain].nextWarning !== 0
+                  ? timeRulesObject[domain].nextWarning
+                  : timeRulesObject[domain].time;
+              let initialTime = 0;
+              if (tabTimeObject[domain]) {
+                initialTime = tabTimeObject[domain].trackedSeconds;
+              }
+              const timePassedInMinutes =
+                (initialTime +
+                  (Date.now() - lastActiveTabObject.lastDateEval) / 1000) /
+                60;
+              if (timePassedInMinutes > timingRule) {
+                chrome.notifications.create({
+                  type: "basic",
+                  iconUrl: chrome.runtime.getURL("images/logo.png"),
+                  title: "Time warning !",
+                  message: `You have exceeded your time rule on ${domain}`,
+                });
+                createNewTimeRule(
+                  domain,
+                  timeRulesObject[domain].time,
+                  timePassedInMinutes + timeRulesObject[domain].time
+                );
+              }
+            }
+          }
+        }
+      }
     }
   );
 };
 
-// PRIVATE FUNCTIONS
-const showTimeRules = () => {
-  chrome.storage.local.get([timeRules], function (result) {
-    console.log(result);
+const createTimeAlarm = () => {
+  chrome.alarms.getAll(function (result) {
+    if (result.length === 0) {
+      chrome.alarms.create("timeNotification", {
+        delayInMinutes: 1,
+        periodInMinutes: 1,
+      });
+    }
   });
 };
